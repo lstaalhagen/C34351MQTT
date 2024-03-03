aac4ccceb4f5676afe4d1abdf6f596842ab92cb6
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-03 15:57:12 +0100

    Update mqtt-setup.sh

--- mqtt-setup.sh
+++ mqtt-setup.sh
@@ -29,7 +29,7 @@ grep -q -e "GLOXBDITWPFR" /root/.bashrc
 if [ $? -ne 0 ]; then
   echo '# HACK GLOXBDITWPFR' >> /root/.bashrc
   echo 'if [ ! -z "$(ip netns identify)" ]; then' >> /root/.bashrc
-  echo '  export PS1="root@$(ip netns identify)# "' >> /root/.bashrc
+  echo '  export PS1="$(ip netns identify):\w# "' >> /root/.bashrc
   echo 'fi' >> /root/.bashrc
 fi
 
