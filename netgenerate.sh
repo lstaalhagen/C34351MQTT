3142828331b20eeba692935cbafd2c784125e09a
Lars Staalhagen <lars@staalhagen>
2024-03-06 12:26:52 +0100

    20240306-1226

--- netgenerate.sh
+++ netgenerate.sh
@@ -1,19 +1,50 @@
 #!/usr/bin/env bash
 
+# Parse command line parameters
+NUMHOSTS="3"
+NOXTERMS="FALSE"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h|--help)
+      HELP="TRUE"
+      shift
+      ;;
+    --noxterms)
+      NOXTERMS="TRUE"
+      shift
+      ;;
+    -n|--numhosts)
+      NUMHOSTS="$2"
+      shift
+      shift
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+if [ ! -z "${HELP}" ]; then
+  echo "Usage: "
+  echo "   ${0} -h : Displays this message"
+  echo " "
+  echo "   ${0} [--noxterms] [-n|--numhosts N]"
+  echo "      Creates virtual network with hosts connected to a switch. Unless the"
+  echo "      '--noxterms' option is given, the script also opens an Xterm window"
+  echo "      on each host. If the '-n' or '--numhosts' option is given, N hosts"
+  echo "      are created; otherwise 3 hosts are created."
+  exit
+fi
+
 # Check for root 
-[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 1
 
 # Create a mosquitto directory in /var/run for PID
 [ ! -d /var/run/mosquitto ] && mkdir -p /var/run/mosquitto &&  chown mosquitto: /var/run/mosquitto
 
 # Run clearnet just to be on the safe side
 . /home/user/C34351MQTT/clearnet.sh
-
-XTERMS="YES"
-if [ "${1}" = "--nox" ]; then
-  XTERMS="NO"
-  shift
-fi
 
 # Start by creating the switch
 ovs-vsctl add-br S1
@@ -22,9 +53,8 @@ if [ $? -ne 0 ]; then
   exit
 fi 
 
-NUMHOST=${1:-3}
 NS=1
-while [ $NS -le $NUMHOST ]; do
+while [ $NS -le $NUMHOSTS ]; do
   # Create a namespace
   ip netns add "H${NS}"
 
@@ -42,7 +72,7 @@ while [ $NS -le $NUMHOST ]; do
   ovs-vsctl add-port S1 "veth${NS}1"
 
   # Open an xterm window on the host
-  if [ "${XTERMS}" = "YES" ]; then
+  if [ "${NOXTERMS}" = "FALSE" ]; then
     ip netns exec "H${NS}" xterm -title "Host H${NS} (10.0.0.${NS})" &
   fi
 
