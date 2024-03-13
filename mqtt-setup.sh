6d52b42ec779aa003ba51807098e9e2cf77d17f9
Lars Staalhagen <lars@staalhagen.dk>
2024-03-13 10:00:14 +0100

    20240313-1000

--- mqtt-setup.sh
+++ mqtt-setup.sh
@@ -6,17 +6,19 @@ REALUSER=${SUDO_USER}
 [ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
 # Download MQTT broker and clients
+echo "Downloading MQTT broker and clients"
 apt-get update
 apt-get -y install mosquitto mosquitto-clients
 
 # Remove the autostart service of mosquitto
+echo "Stopping the mosquitto service"
 systemctl stop mosquitto.service
 systemctl mask mosquitto.service
 
 # Hack to get a better default font size in Xterm windows
 XRESOURCES=/home/${REALUSER}/.Xresources
 if [ -s $XRESOURCES ]; then
-  grep -q -e "^xterm*faceName:"
+  grep -q -e "^xterm\*faceName:" $XRESOURCES
   if [ $? -ne 0 ]; then
     echo "xterm*faceName: Monospace" >> $XRESOURCES
     echo "xterm*faceSize: 14" >> $XRESOURCES
