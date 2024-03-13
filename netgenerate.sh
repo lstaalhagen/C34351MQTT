e9d4789a2aaf8e80cbc5f77b319a0f64ad422fb1
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-13 09:05:04 +0100

    Update netgenerate.sh

--- netgenerate.sh
+++ netgenerate.sh
@@ -59,9 +59,6 @@ fi
 # Check for root 
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 1
 
-# Create a mosquitto directory in /var/run for PID
-[ ! -d /var/run/mosquitto ] && mkdir -p /var/run/mosquitto &&  chown mosquitto: /var/run/mosquitto
-
 # Run clearnet just to be on the safe side
 [ -s ./clearnet.sh ] && . ./clearnet.sh
 
